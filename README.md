d766fff77a7ea26a2f57a80f0947b863ced731ba
Mustafa Buda <mustafabuda@gmail.com>
2012-12-08 23:19:43 +0000

    Proper setup of django

--- README.md
+++
@@ -1,8 +0,0 @@
-ideal-web
-=========
-
-Website for quickshop.
-
-For Django documentation see here: https://docs.djangoproject.com/en/dev/
-
-For a tutorial, see here: https://docs.djangoproject.com/en/dev/intro/tutorial01/
--- idealWeb/urls.py
+++ idealWeb/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -6,8 +6,8 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ideal_web.views.home', name='home'),
-    # url(r'^ideal_web/', include('ideal_web.foo.urls')),
+    # url(r'^$', 'idealWeb.views.home', name='home'),
+    # url(r'^idealWeb/', include('idealWeb.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
